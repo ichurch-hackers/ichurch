2a451478fc293fb18fc3d610b5423684432098a8
Brad Kingon <bkingon@gmail.com>
2014-06-16 15:05:44 +0200

    add search bar in layout for songs

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,4 +3,26 @@ class Song < ActiveRecord::Base
 
   validates :title, :content, presence: true
   validates :key, inclusion: CHROMATIC_SCALE
+
+  include PgSearch
+  pg_search_scope :full_search,
+                  against:  [:title, :content, :author, :version_comment],
+                  using:  {
+                    tsearch: {
+                      dictionary: "english",
+                      prefix:   true,
+                      any_word: true
+                    }
+                  },
+                  ignoring: :accents
+
+  def self.search(query)
+    return all if query.blank?
+    full_search(query)
+  end
+
+  def first_line
+    lines = self.content.split("\n")
+    lines[2]
+  end
 end
