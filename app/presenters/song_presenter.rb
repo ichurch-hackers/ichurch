3f22a0db431178c374289ed78adf4991fe0b3c3b
Kevin McKelvin <kmckelvin@gmail.com>
2014-06-19 23:29:12 +0200

    Song PDF export with transposing

--- app/presenters/song_presenter.rb
+++ app/presenters/song_presenter.rb
@@ -1,4 +1,6 @@
 class SongPresenter < SimpleDelegator
+  attr_reader :transpose_distance
+
   def initialize(song, transpose_distance)
     super(song)
     @transpose_distance = transpose_distance.to_i
@@ -13,7 +15,7 @@ class SongPresenter < SimpleDelegator
   end
 
   def to_chord_lines
-    super(transpose: @transpose_distance)
+    super(transpose: transpose_distance)
   end
 
   def transpose_distances
