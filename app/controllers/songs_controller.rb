a6922aa5ec551b2dbc8f74546e588a7fffb72a00
Kevin McKelvin <kmckelvin@gmail.com>
2014-06-20 23:39:11 +0200

    Re-enable authentication

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,5 @@
 class SongsController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :set_song, only: [:show, :edit, :update, :destroy]
 
   # GET /songs
